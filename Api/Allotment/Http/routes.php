6e9140c15204739b002cddd553527f848b4d2d24
Peter Mein <peter@rapide.software>
2017-08-01 17:18:13 +0200

    Removed Allotment

--- Api/Allotment/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'allotment', 'namespace' => 'Api\Allotment\Http\Controllers'], function()
-{
-    Route::get('/', 'AllotmentController@index');
-});
--- Api/Allotment/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('allotment::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('allotment.name') !!}
-    </p>
-@stop
